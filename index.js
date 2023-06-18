b087a8f2a8555c7057eaa121d154af76cf730531
Killereq-PL <adam.kotek33@gmail.com>
2023-06-18 21:53:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express();
 const strings = ['It\'s getting dark...', 'My day was productive...', 'I\'m happy to be helping science...', 'It\'s very lonely here...', 'I hope this day isn\'t my last...', 'The sunset is beautiful...', 'Martian soil is my home...']; // Replace with your list of strings
 
 app.get('/mars-api', (req, res) => {
+  res.statusCode = 200;
   const randomIndex = Math.floor(Math.random() * strings.length);
   const randomString = strings[randomIndex];
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ string: randomString }));
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(randomString);
 });
 
 const port = process.env.PORT || 3000;
