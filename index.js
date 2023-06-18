a0c118151ae3418e32c63ee09be1bf8e1c9441b0
Killereq-PL <adam.kotek33@gmail.com>
2023-06-18 14:16:59 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const strings = ['string1', 'string2', 'string3', 'string4']; // Replace with your list of strings
+
+app.get('/random-string', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * strings.length);
+  const randomString = strings[randomIndex];
+  res.json({ string: randomString });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
